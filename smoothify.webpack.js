e3bf06be58ca2922c9d0c8d75dc3423fade53dae
James Homer <jameshomer85@gmail.com>
2016-06-06 17:04:36 +0100

    fix webpack

--- smoothify.webpack.js
+++ smoothify.webpack.js
@@ -1,7 +1,7 @@
-import Hamster from 'expose?Hamster!hamsterjs';
-import TweenMax from 'imports?define=>false!gsap/src/uncompressed/TweenMax';
-import 'gsap/src/uncompressed/plugins/ScrollToPlugin';
+require('expose?Hamster!hamsterjs');
+require('imports?define=>false!gsap/src/uncompressed/TweenMax');
+require('gsap/src/uncompressed/plugins/ScrollToPlugin');
 
-import Smoothify from 'expose?Smoothify!./src/smoothify';
+var Smoothify = require('expose?Smoothify!babel?presets[]=es2015!./src/smoothify');
 
-export default Smoothify;
+module.exports = Smoothify;
